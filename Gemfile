1fb69af294f4f48e1bab529b5c063f9eb0e9e482
kai-munekuni <kai-munekuni@M-Kai-MunekunisMacBook-Pro.local>
2019-03-01 00:26:30 +0900

    initial

---
+++ Gemfile
@@ -0,0 +1,23 @@
+source "https://rubygems.org"
+
+
+gem 'rubocop'
+gem 'ruby-debug-ide'
+gem 'debase'
+gem 'rcodetools'
+gem 'fastri'
+
+gem 'rake'
+gem 'sinatra'
+gem 'sinatra-contrib'
+gem 'sinatra-websocket'
+gem "activerecord"
+gem 'sinatra-activerecord'
+gem 'pry'
+group :development do
+  gem 'sqlite3', '~>1.3.13'
+end
+
+group :production do
+  gem 'pg', '~> 0.18'
+end
---
+++ app.rb
@@ -0,0 +1,40 @@
+require 'bundler/setup'
+Bundler.require
+require 'sinatra/reloader' if development?
+require 'sinatra-websocket'
+
+set :server, 'thin'
+set :sockets, []
+enable :sessions
+get '/' do
+  erb :index
+end
+
+get '/horizontal' do
+
+end
+
+get '/vertical' do
+  settings.sockets.each do |s|
+        s.send("hoge")
+    end
+    return 'hoge'
+end
+
+get '/websocket' do
+  if request.websocket?
+    request.websocket do |ws|
+      ws.onopen do
+        settings.sockets << ws
+      end
+      ws.onmessage do |msg|
+        settings.sockets.each do |s|
+          s.send(msg)
+        end
+      end
+      ws.onclose do
+        settings.sockets.delete(ws)
+      end
+    end
+  end
+end
