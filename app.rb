fc1ff66bd3f4fc747b2cae37d9810f1002ee1980
amishiratori <shiratoriami@gmail.com>
2019-02-28 23:44:01 +0000

    create lp

--- app.rb
+++ app.rb
@@ -2,12 +2,33 @@ require 'bundler/setup'
 Bundler.require
 require 'sinatra/reloader' if development?
 require 'sinatra-websocket'
+require './models.rb'
 
 set :server, 'thin'
 set :sockets, []
 enable :sessions
+
+before do
+  if Count.all.length == 0
+    Count.create(number: 0)
+  end
+end
+
+
 get '/' do
   erb :index
+end
+
+get '/lp' do
+  erb :top
+end
+
+post '/join' do
+  count = Count.first
+  count.number = count.number + 1
+  count.save
+  session[:number] = count.number
+  redirect '/'
 end
 
 get '/horizontal' do
