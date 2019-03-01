231aa71e4dfb735e9999d3db916a26d122418a58
amishiratori <shiratoriami@gmail.com>
2019-03-01 03:59:15 +0000

    adjust for heroku

--- models.rb
+++ models.rb
@@ -2,7 +2,7 @@ require 'bundler/setup'
 Bundler.require
 
 if development?
-ActiveRecord::Base.establish_connection('sqlite3:db/development.db')
+ActiveRecord::Base.establish_connection(ENV['DATABASE_URL'] || 'sqlite3:db/development.db')
 end
 
 
